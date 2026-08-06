f33003657126d4f572015fe70e143cc232ebdeaa
Arjun.O <arjundlm10@gmail.com>
2026-08-06 22:21:44 +0530

    feat:Added Player as a rectangle and draw it on GameWindow

--- src/core/GamePanel.java
+++ src/core/GamePanel.java
@@ -9,13 +9,18 @@ import javax.swing.Timer;
 
 import java.awt.event.ActionListener;
 
+import player.Player;
+
 public class GamePanel extends JPanel{
 
     private Timer gameTimer;
+    private Player player;
 
     public GamePanel(){
         setPreferredSize(new Dimension(960,640));
         setBackground(Color.BLACK);
+
+        this.player = new Player(100, 100);
 
         this.gameTimer = new Timer(16, e -> {
             update();
@@ -33,6 +38,10 @@ public class GamePanel extends JPanel{
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
+
+        g.setColor(player.getColor());
+        g.fillRect(player.getX(), player.getY(), player.getWidth(), player.getHeight());
+       
     }
     
 }
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,7 +1,49 @@
 package player;
 
-
+import java.awt.Color;
 
 public class Player {
-    
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Color color;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+        this.width = 32;
+        this.height = 32;
+        this.color = Color.CYAN;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
 }
