0766615cb60e0792244fe29cca69414214f5ab01
Arjun.O <arjundlm10@gmail.com>
2026-08-06 23:05:37 +0530

    feat: Wired Up KeyInput Detection to made the player Able to move around

--- src/core/GamePanel.java
+++ src/core/GamePanel.java
@@ -10,17 +10,23 @@ import javax.swing.Timer;
 // import java.awt.event.ActionListener;
 
 import player.Player;
+import input.InputSystem;
 
 public class GamePanel extends JPanel{
 
     private Timer gameTimer;
     private Player player;
+    private InputSystem inputSystem;
 
     public GamePanel(){
         setPreferredSize(new Dimension(960,640));
         setBackground(Color.BLACK);
 
         this.player = new Player(100, 100);
+
+        inputSystem = new InputSystem(this);
+        setFocusable(true);
+        requestFocusInWindow();
 
         this.gameTimer = new Timer(16, e -> {
             update();
@@ -32,6 +38,21 @@ public class GamePanel extends JPanel{
     }
 
     private void update(){
+        final int speed = 4;
+
+        if(this.inputSystem.isUp()){
+            player.setY(player.getY() - speed);
+        }
+        if(this.inputSystem.isDown()){
+            player.setY(player.getY() + speed);
+        }
+        if(this.inputSystem.isLeft()){
+            player.setX(player.getX() - speed);
+        }
+        if(this.inputSystem.isRight()){
+            player.setX(player.getX() + speed);
+        }
+        
 
     }
 
